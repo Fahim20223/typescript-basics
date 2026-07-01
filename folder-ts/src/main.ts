8bfd0fad496168ba406c2334673c666fc2d8a82f
Fahim2023 <fahimshahrier2023@gmail.com>
2026-07-01 13:24:30 +0600

    just clarified the void funciton that we not return anyting that calls void

--- folder-ts/src/main.ts
+++ folder-ts/src/main.ts
@@ -1,5 +1,16 @@
-const ageCal = (name: string, age: number) => {
-  return `Hi ${name}, you are ${age} old`;
+const ageCal = (name: string = "fahim", age: number = 30): void => {
+  // return `Hi ${name}, you are ${age} old`;
+  // return age;
+  // return name;
+  console.log(`Hi ${name}`);
 };
 
-ageCal("fahim", 40);
+ageCal();
+
+// if the function returns nothing then we call it void .... we can see it line number 1 where that we calls void because inside the function we just console.log() but not returns ...
+
+const ageCal2 = (name: string = "fahim", age: number = 30): string => {
+  return `hi ${name} , you are age is ${age}`;
+};
+
+ageCal2();
